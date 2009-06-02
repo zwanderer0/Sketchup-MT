a6a38248ad67a5b8f38f01dd655dbe08b33c9ed0
anirudhsharma.crypto@gmail.com <anirudhsharma.crypto@gmail.com@0b448cae-322c-11de-8e0c-53a27eea117e>
2009-06-02 09:08:49 +0000

    initial directory structure. extension module call.tuioconn.rb experiments

---
+++ SketchUpMT.rb
@@ -0,0 +1,10 @@
+     # Create an entry in the SketchUp Extension list that loads a script called
+     # tuioconn.rb.
+     require 'sketchup.rb'
+     require 'extensions.rb'
+
+     sketchupmt_extension = SketchupExtension.new "TUIO Conn", "SketchUpMT/tuioconn.rb"
+     sketchupmt_extension.version = '0.1'
+     sketchupmt_extension.description = "Use Google SketchUp with touch input on Multitouch table"
+     sketchupmt_extension.copyright = "NUIGroup Community, done as a part of GSoC 2009"
+     Sketchup.register_extension sketchupmt_extension, true
