622e4d9944acc93a0d2cc266cf67b619fb947af2
anirudhsharma.crypto@gmail.com <anirudhsharma.crypto@gmail.com@0b448cae-322c-11de-8e0c-53a27eea117e>
2009-06-08 23:39:08 +0000

    modifications with extension module functioning and iconsets loading. Mostly UI stuff

--- SketchUpMT.rb
+++ SketchUpMT.rb
@@ -1,15 +1,38 @@
    
 
+=begin
+
+yea
+
+=end
 
 
-
-   # Create an entry in the SketchUp Extension list that loads a script called
-     # tuioconn.rb.
-     require 'sketchup.rb'
-     require 'extensions.rb'
+  # Create an entry in the SketchUp Extension list that loads a script called
+  # tuioconn.rb.
+require 'sketchup.rb'
+require 'extensions.rb'
 	 
 	 
 	 $sketchupmt_toolbar=UI::Toolbar.new "Multitouch"  #j9
+	 
+	
+	 
+	s1=UI::Command.new("NUIGroup Site") { UI.openURL("http://www.nuigroup.com/")}
+	s1.small_icon="SketchUpMT/buttonset/nuiicon.png"
+	s1.large_icon="SketchUpMT/buttonset/nuiiconlarge.png"
+	s1.menu_text="NUIGroup"
+	s1.tooltip="Go to the NUIgroup website"
+	$sketchupmt_toolbar.add_item(s1)
+	
+	s2=UI::Command.new("Multitouch Navigation") { UI.openURL("##CHANGE ACTION HERE/#{email}")} #IMPORTANT J908
+	s2.small_icon="SketchUpMT/buttonset/orbiting.png"
+	s2.large_icon="SketchUpMT/buttonset/mysmustardiconlarge.png"
+	s2.menu_text="Orbit and Pan"
+	s2.tooltip="Gestures in lib"
+	$sketchupmt_toolbar.add_item(s2)
+	
+	$smustard_toolbar.show
+
 
 
      sketchupmt_extension = SketchupExtension.new "TUIO Conn", "SketchUpMT/tuioconn.rb"
