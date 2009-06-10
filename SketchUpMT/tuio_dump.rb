6d0938020b967f2ba3975d69bdb61787c90155b0
anirudhsharma.crypto@gmail.com <anirudhsharma.crypto@gmail.com@0b448cae-322c-11de-8e0c-53a27eea117e>
2009-06-10 11:24:19 +0000

    updates TUIO client

---
+++ SketchUpMT/tuio_dump.rb
@@ -0,0 +1,32 @@
+require File.join( File.dirname( __FILE__ ), '..', 'lib', 'tuio_client' )
+
+
+@tc = TuioClient.new
+
+@tc.on_object_creation do | to |
+  puts "New TUIO Object at x: #{to.x_pos}, y: #{to.y_pos}"
+end
+
+@tc.on_object_update do | to |
+  puts "Updated TUIO Object #{to.fiducial_id} at x: #{to.x_pos}, y: #{to.y_pos}"
+end
+
+@tc.on_object_removal do | to |
+  puts "Removed TUIO Object #{to.fiducial_id}"
+end
+
+
+@tc.on_cursor_creation do | to |
+  puts "New TUIO Cursor at x: #{to.x_pos}, y: #{to.y_pos}"
+end
+
+@tc.on_cursor_update do | to |
+  puts "Updated TUIO Cursor #{to.session_id} at x: #{to.x_pos}, y: #{to.y_pos}"
+end
+
+@tc.on_cursor_removal do | to |
+  puts "Removed TUIO Cursor #{to.session_id}"
+end
+
+@tc.start
+sleep
